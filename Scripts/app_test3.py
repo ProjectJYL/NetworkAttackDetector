66fc7650da2b59b7cb26df79beee79adb9b5bee1
ProjectJYL <jianyet.lee@gmail.com>
2017-11-20 16:10:02 -0500

    modified the app to include two lines in a plot and ticking

--- Scripts/app_test3.py
+++ Scripts/app_test3.py
@@ -6,73 +6,55 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.animation as animation
 from matplotlib import style
-import matplotlib.pyplot as plt
 
 style.use('ggplot')
 LARGE_FONT = ("Verdana", 12)
         
-lines = []
-current_index = 1;
-
-##f = Figure(figsize=(5,5), dpi=100)
-##a = f.add_subplot(111)
-fig, ax = plt.subplots(figsize=(15,5))
-xdata, y1data, y2data = [],[],[]
-ln1, ln2= plt.plot([], [], [], [], animated=True)
+current_index = 0;
+fig = Figure(figsize=(15,5), dpi=100)
+fig.subplots_adjust(left=0.07,bottom=0.1,right=0.95,top=0.95)
+a = fig.add_subplot(111)
 
 def init():
-##    ax.set_xlim(0, 1000)
-    ax.set_ylim(-2, 2)
-    ax.set_xlabel("xdata")
-    ax.set_ylabel("ydata")
-    ln1.set_label("line1")
-    ln2.set_label("line2")
-    ax.set_xbound(0,1000)
-    plt.legend()
-    return ln1,ln2
+    a.set_ylim(-2,2)
+    a.set_xlim(auto=True)
+    a.set_xlabel("xdata")
+    a.set_ylabel("ydata")
+    return a.lines
 
 def update(frame):
-    global lines
     global current_index
+    x,y1,y2 = 0,0,0
+    pullData = open("testname",'r').read()
+    dataArray = pullData.split('\n')
+    if(current_index > len(dataArray)):
+        return a.lines
+    
+    xdata, y1data, y2data = [],[],[]        
+    for eachline in dataArray[:current_index]:
+        if(len(eachline)>1):
+            x,y1,y2 = eachline.split(',')
+            xdata.append(int(x))
+            y1data.append(float(y1))
+            y2data.append(float(y2))
 
-    if(len(lines) < 1):
-        print('Err:file data is not read into the array.')
-        return ln1, ln2
-
-    if(current_index >= len(lines)):
-        print('EOF is reached. No new data to be plotted.')
-        return ln1, ln2
-        
-    x,y1,y2 = lines[current_index].split(',')
-    xdata.append(int(x))
-    y1data.append(float(y1))
-    y2data.append(float(y2))
-    ln1.set_data(xdata,y1data)
-    ln2.set_data(xdata,y2data)
-##    upperx = 100+int(x)
-##    lowerx = int(x)-1
-##    ax.set_xlim(left=lowerx,right=upperx)
+    a.clear()
+    a.plot(xdata,y1data,'r', label= 'line1')
+    a.plot(xdata,y2data,'g', label= 'line2')
     current_index = current_index + 1;
-    return ln1, ln2
-
+    return a.lines
         
 def openfilecb():
-    global lines
-    global current_index
-    global xdata,y1data, y2data
+    global current_index,xdata,y1data,y2data
     
-    filename = tk.filedialog.askopenfilename()
-    fo = open(filename,'r')
+    filename = tk.filedialog.askopenfilename()    
     lines = []
     lines = fo.readlines();
     fo.close()
     if(fo.closed):
         current_index = 1
-        xdata,y1data, y2data = [],[], []
-        plt.clf()
-        fig.clear()
-        ax.set_xlim(0,100,auto=True)
         print('File "%s" is opened and read successfully' %filename)
+    
     return
 
 def callback():
@@ -147,19 +129,11 @@ class StartPage(tk.Frame):
         button.pack()
 
 class GraphPage(tk.Frame):
+    global canvas
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Graph Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)
-
-##        button1 = ttk.Button(self, text="Back to Homepage", command=
-##                             lambda: controller.showframe(StartPage))
-##        button1.pack()
-
-##        f = Figure(figsize=(5,5), dpi=100)
-##        a = f.add_subplot(111)
-##        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
-        
+        label.pack(pady=10,padx=10)        
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.show()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
@@ -170,23 +144,10 @@ class GraphPage(tk.Frame):
                            
 def qf(quickPrint):
     print(quickPrint)
-    
-##class PageThree(tk.Frame):
-##    def __init__(self,parent,controller):
-##        tk.Frame.__init__(self,parent)
-##        label = tk.Label(self, text ="Graph Page!", font=LARGE_FONT)
-##        label.pack(pady=10,padx=10)
-##
-##        button1 = ttk.Button(self, text="Back to Home",
-##                             command=lambda: controller.show_frame(StartPage))
-##        button1.pack()
 
 app = SeaofBTCapp()
-ani = animation.FuncAnimation(fig, update, frames=list(range(0,1000)),
-                              interval=10)
-##ani = animation.FuncAnimation(fig, update, init_func=init,
-##                              blit=True, interval=1000)
-
+ani = animation.FuncAnimation(fig, update, init_func=init,blit = True,
+                              interval=1000)
 app.mainloop()
 
         
