a5170c54c66dc72ceefadfac5ad9c89855d90598
ProjectJYL <jianyet.lee@gmail.com>
2017-11-24 18:51:21 -0500

    included picker and annotation

--- Scripts/app_test2.py
+++ Scripts/app_test2.py
@@ -6,71 +6,100 @@ import tkinter as tk
 from tkinter import ttk
 import matplotlib.animation as animation
 from matplotlib import style
-import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+import numpy as np
 
 style.use('ggplot')
 LARGE_FONT = ("Verdana", 12)
         
 lines = []
-current_index = 1;
+current_index = 0;
+filename = ""
 
 fig = Figure(figsize=(15,5), dpi=100)
-a = fig.add_subplot(111)
-a.plot([],[],[],[])
-fig.legend(a.get_lines(),("line 1","line 2"),"upper right")
+afig = fig.add_subplot(111)
+afig.set_xlabel("xdata")
+afig.set_ylabel("ydata")
+afig.set_adjustable('datalim')
+afig.plot([],[],'r',[],[],'g')
+afig.xaxis.set_animated(True)
+afig.set_ylim(-1,1)
+fig.legend(afig.get_lines(),("line 1","line 2"),"upper right")
 
-def init():
-    a.set_ylim(-2,2)
-    a.set_xlim(0,1000)
-    a.set_xlabel("xdata")
-    a.set_ylabel("ydata")
-    return a.lines
+def line_picker(line, mouseevent):
+        """
+        find the points within a certain distance from the mouseclick in
+        data coords and attach some extra attributes, pickx and picky
+        which are the data points that were picked
+        """
+        if mouseevent.xdata is None:
+            return False, dict()
+        xdata = line.get_xdata()
+        ydata = line.get_ydata()
+        maxd = 0.05
+        d = np.sqrt((xdata - mouseevent.xdata)**2. + (ydata - mouseevent.ydata)**2.)
 
+        ind = np.nonzero(np.less_equal(d, maxd))
+        if len(ind):
+            pickx = np.take(xdata, ind)
+            picky = np.take(ydata, ind)
+            props = dict(ind=ind, pickx=pickx, picky=picky)
+            return True, props
+        else:
+            return False, dict()
+
+def onpick(event):
+    print('(%s,%s)' % (str(event.pickx), str(event.picky)))
+    
 def update(frame):
     global current_index
-    global lines
+    global filename
 
-    if(len(lines)<1):
-        return a.lines
+    if(len(filename) < 1):
+        return afig.lines
+
+    data = open(filename,'r').read()    
+    dataline = data.split('\n')
+    if(current_index > len(dataline) ):
+        return afig.lines
     
     xdata, y1data, y2data = [],[],[]        
-    for eachline in lines[:current_index]:
+    for eachline in dataline[:current_index]:
         if(len(eachline)>1):
             x,y1,y2 = eachline.split(',')
             xdata.append(int(x))
             y1data.append(float(y1))
-            y2data.append(float(y2))
-    
-    a.plot(xdata,y1data,'r', label= 'line1')
-    a.plot(xdata,y2data,'g', label= 'line2')
+            y2data.append(float(y2))           
+
+    afig.clear()
+    afig.plot(xdata,y1data,'r',
+        xdata,y2data,'g',picker=line_picker)
+    for i in xdata:
+        afig.annotate(s="(%s,%s)"%(xdata[i],y1data[i]),
+                      xy=(xdata[i],y1data[i]),
+                      xycoords="data")
+        afig.annotate(s="(%s,%s)"%(xdata[i],y2data[i]),
+                      xy=(xdata[i],y2data[i]),
+                      xycoords="data")
+
     current_index = current_index + 1;
-    return a.lines
+    return afig.lines
         
 def openfilecb():
-    global lines
-    global current_index
-    global xdata,y1data, y2data
+    global filename, current_index, xdata,y1data, y2data
     
     filename = tk.filedialog.askopenfilename()
-    fo = open(filename,'r')
-    lines = []
-    lines = fo.readlines();
-    fo.close()
-    if(fo.closed):
-        current_index = 1
-        xdata,y1data, y2data = [],[], []
-        print('File "%s" is opened and read successfully' %filename)
-        
+    current_index = 0
+    xdata,y1data, y2data = [],[], []      
     return
 
 def callback():
     filename = tk.filedialog.askopenfilename()
-    print(filename)
     return
 
 def aboutappcb():
-    title = "About Network Attack Detector"
-    message="Version 1.0                        "
+    title = "About"
+    message="Network Attack Detector. Version 1.0"
     tk.filedialog.messagebox.showinfo(title,message)
     return
 
@@ -134,24 +163,27 @@ class StartPage(tk.Frame):
                            command = lambda: qf("Check it out, i'm passing vars"))
         button.pack()
 
-class GraphPage(tk.Frame):
+class GraphPage(tk.Frame):    
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
         label = tk.Label(self, text="Graph Page", font=LARGE_FONT)
-        label.pack(pady=10,padx=10)        
+        label.pack(pady=10,padx=10)
+##        labelvar = tk.StringVar()
+##        xylabel = tk.Label(self, textvariable=labelvar)
+##        xylabel.pack()
+##        labelvar.set("x,y")
         canvas = FigureCanvasTkAgg(fig, self)
         canvas.show()
         canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand=True)
         toolbar = NavigationToolbar2TkAgg(canvas, self)
         toolbar.update()
         canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
+        canvas.mpl_connect("pick_event", onpick)
         
-                           
-def qf(quickPrint):
-    print(quickPrint)
+
 
 app = SeaofBTCapp()
-ani = animation.FuncAnimation(fig, update, init_func=init, blit = True,
+ani = animation.FuncAnimation(fig, update,blit=True,
                               interval=10)
 app.mainloop()
 
